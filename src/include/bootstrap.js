e63cf041efa9626bff856b6ef3110ea36cfa7a05
egerber <emanuelgerber@web.de>
2018-01-03 16:42:05 +1100

    importing only css

--- src/include/bootstrap.js
+++ src/include/bootstrap.js
@@ -1,4 +1,4 @@
-import * as $ from 'jquery';
+import 'jquery';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
-window.jQuery=$;
-require('bootstrap'); //for some reason does not work with import
+
+import 'bootstrap';
--- src/include/checkBoxTree.js
+++ src/include/checkBoxTree.js
@@ -1,3 +1,3 @@
 import '../../node_modules/react-checkbox-tree/lib/react-checkbox-tree.css';
-import '../../node_modules/react-checkbox-tree/src/less/react-checkbox-tree.less';
-import '../../node_modules/react-checkbox-tree/src/scss/react-checkbox-tree.scss';
+//import '../../node_modules/react-checkbox-tree/src/less/react-checkbox-tree.less';
+//import '../../node_modules/react-checkbox-tree/src/scss/react-checkbox-tree.scss';
